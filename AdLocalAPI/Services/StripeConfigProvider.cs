0429dd51d957e6c0341114660834ef1c64c88c6a
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-02-02 19:47:19 -0600

    ....

---
+++ AdLocalAPI/Services/StripeConfigProvider.cs
@@ -0,0 +1,33 @@
+﻿using AdLocalAPI.Constants;
+using AdLocalAPI.Models;
+
+namespace AdLocalAPI.Services
+{
+    public class StripeConfigProvider
+    {
+        public string SecretKey { get; private set; } = null!;
+        public string PublishableKey { get; private set; } = null!;
+        public decimal CommissionPercentage { get; private set; }
+        public decimal CommissionFixed { get; private set; }
+
+        public void Load(IEnumerable<ConfiguracionSistema> configs)
+        {
+            SecretKey = configs
+                .First(x => x.Key == ConfiguracionKeys.StripeSecretKey)
+                .Val;
+
+            PublishableKey = configs
+                .First(x => x.Key == ConfiguracionKeys.StripePublishableKey)
+                .Val;
+
+            CommissionPercentage = decimal.Parse(
+                configs.First(x => x.Key == ConfiguracionKeys.StripeCommissionPercentage).Val
+            );
+
+            CommissionFixed = decimal.Parse(
+                configs.First(x => x.Key == ConfiguracionKeys.StripeCommissionFixed).Val
+            );
+        }
+    }
+
+}
