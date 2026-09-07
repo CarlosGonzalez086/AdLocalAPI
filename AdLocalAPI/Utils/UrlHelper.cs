5265faa682747be6e6766565c2dd4c99b5804fd0
Jose Carlos Gonzalez <jgonzalez@flexor.mx>
2026-09-07 12:01:44 -0600

    ...

--- AdLocalAPI/Utils/UrlHelper.cs
+++ AdLocalAPI/Utils/UrlHelper.cs
@@ -5,7 +5,7 @@
         public static string GenerarLinkCambioPassword(string token, bool esProduccion, string userType)
         {
             var baseUrl = esProduccion
-                ? "https://adlocal.jcarlosgonzalez086.workers.dev/usuario/"
+                ? "https://panel.adlocal.store/usuario/"
                 : "http://localhost:5173/usuario";
 
             return $"{baseUrl}/restablecer-contrasena/{token}/{userType}";
@@ -13,7 +13,7 @@
         public static string GenerarLinkNuevoColaborador(string token, bool esProduccion)
         {
             var baseUrl = esProduccion
-                ? "https://adlocal.jcarlosgonzalez086.workers.dev/usuario/"
+                ? "https://panel.adlocal.store/usuario/"
                 : "http://localhost:5173/usuario";
 
             return $"{baseUrl}/nuevo-colaborador/{token}";
